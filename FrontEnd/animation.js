f40f6bdb4f5a4a25499358205cf433d1a896ab63
Sparky-gg <sparkydk5758967@gmail.com>
2024-09-13 08:26:13 +0545

    Whatever

--- FrontEnd/animation.js
+++ FrontEnd/animation.js
@@ -9,6 +9,8 @@ tl.to("nav", {
   // borderBottomColor: "#000000",
   boxShadow: "0 0 20px -3px #000000",
   duration: 1,
+  backgroundColor: "#001f3fe2",
+  backdropFilter: "blur(10px)",
   height: "12%",
   scrollTrigger: {
     trigger: "nav",
@@ -28,21 +30,20 @@ window.onload = function () {
   hero.style.marginTop = navHeight - 8 + "px"; // applies the height as margin-top
 };
 
-
 // Hero Section's Main quote animation
 
-tl.from(".where",{
-  x : "-300px",
+tl.from(".where", {
+  x: "-300px",
   opacity: 0,
-  duration : 1
-})
+  duration: 1,
+});
 
-tl.from(".eve-pic",{
-  x : "300px",
-  opacity : 0,
-  duration : 1,
-  scale: 0.5
-})
+tl.from(".eve-pic", {
+  x: "300px",
+  opacity: 0,
+  duration: 1,
+  scale: 0.5,
+});
 
 tl.from(".tells-a-story", {
   x: "-300px",
@@ -50,3 +51,73 @@ tl.from(".tells-a-story", {
   duration: 1,
 });
 
+
+// Animation for hover on images of hero section
+
+// const naturePicDivEl = document.querySelector("#nature-pic-div");
+// const eventsPicDivEl = document.querySelector("#events-pic-div");
+// const cinematicPicDivEl =document.querySelector("#cinematic-pic-div");
+// const natureHiddenTxtEl = document.querySelector("#nature-hidden-txt");
+// const eventHiddenTxtEl = document.querySelector("#event-hidden-txt");
+// const cinematicHiddenTxtEl = document.querySelector("#cinematic-hidden-txt");
+
+// // Nature Images Div
+
+// naturePicDivEl.addEventListener("mouseover",() => {
+//   natureHiddenTxtEl.style.bottom = "50%";
+//   console.log("Nature Lover");
+// })
+
+// imageDivEl.addEventListener('mouseout',() => {
+//   natureHiddenTxtEl.style.bottom = "-100%"
+// })
+
+// // Wedding and Cinematic Images Div
+
+// eventsPicDivEl.addEventListener("mouseover",() => {
+//   console.log("I love wedding ko masu bhat alot");
+// })
+
+// cinematicPicDivEl.addEventListener("mouseover",() => {
+//   console.log("I will shoot a cinema fs");
+// })
+
+
+  // Function to show the hidden text
+  function showText(el) {
+    const hiddenTxt = el.querySelector(".hidden-hover-txt");
+    const imageEl = el.querySelector('.pic-img');
+
+    gsap.to(hiddenTxt, {
+      bottom: "50%", // Animate to show the text
+      duration: 0.3, // Duration of the animation
+    });
+
+    gsap.to(imageEl,{
+      backdropFilter : 'blur(5px)'
+    })
+  }
+
+  function hideText(el) {
+    const hiddenTxt = el.querySelector(".hidden-hover-txt");
+    const imageEl = el.querySelector('.pic-img');
+
+    gsap.to(hiddenTxt, {
+      bottom: "-50px", // Animate to hide the text
+      duration: 0.3, // Duration of the animation
+    });
+
+    gsap.to(imageEl, {
+      backdropFilter: "blur(0px)",
+    });
+  }
+
+
+  // Select all image div elements
+  const imgDivs = document.querySelectorAll('.img-div');
+
+  // Attach event listeners to each image div
+  imgDivs.forEach(imgDiv => {
+    imgDiv.addEventListener('mouseover', () => showText(imgDiv));
+    imgDiv.addEventListener('mouseout', () => hideText(imgDiv));
+  });
